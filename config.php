899550cce937d7c5426dc5d2a7bfa03c0854c2be
REise13 <55093951+REise13@users.noreply.github.com>
2021-09-23 18:09:20 +0300

    Update config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// db variables
+define('DBSERVER', 'localhost');
+define('DBUSER', 'root');
+define('DBPASSWORD', 'YpUVMwmR8)');
+define('DBNAME', 'baseddc');
+
+// connect to MySQL database
+$dbconn = mysqli_connect()
+
+?>
