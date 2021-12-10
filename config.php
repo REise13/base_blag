1840b753cdc5b3fd70a83561f8fd0e1a26766bbd
REise13 <55093951+REise13@users.noreply.github.com>
2021-12-10 14:10:49 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,16 @@
 <?php
-define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'YpUVMwmR8)');
+define('DB_SERVER', '91.223.123.104');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'baseDdc)');
 define('DB_NAME', 'baseddc');
 
 // подключение к базе данных
 try {
-    $con = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $con = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD, array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+      ));
+    // $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
